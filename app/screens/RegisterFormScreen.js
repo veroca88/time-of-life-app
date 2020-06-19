4365ac2bcc42a5b8a74b6aebedf78a1ab37e7815
veroca88 <veronicadonoso1506@gmail.com>
2020-06-18 21:08:06 -0400

    Install YUp to create validationSchema

---
+++ app/screens/RegisterFormScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+
+function RegisterFormScreen(props) {
+  return <Screen></Screen>;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default RegisterFormScreen;
