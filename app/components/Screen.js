ef930c2c9cd01f596f26718133de2723980c668b
veroca88 <veronicadonoso1506@gmail.com>
2020-06-18 21:05:39 -0400

    Create screen component with statusBarHeight

---
+++ app/components/Screen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Constants from "expo-constants";
+import { StyleSheet, SafeAreaView } from "react-native";
+
+function Screen({ children }) {
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+});
+
+export default Screen;
