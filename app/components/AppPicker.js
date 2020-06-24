7739a17381dd66c62518f8eb881175b1221e8807
veroca88 <veronicadonoso1506@gmail.com>
2020-06-23 23:35:18 -0400

    Add AppPicker in form Picker

---
+++ app/components/AppPicker.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+  FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+import AppText from "./AppText";
+import PickerItem from "./PickerItem";
+import Screen from "./Screen";
+
+function AppPicker({
+  icon,
+  items,
+  numberOfColumns,
+  onSelectItem,
+  PickerItemComponent = PickerItem,
+  placeholder,
+  selectedItem,
+  width = "100%",
+}) {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={[styles.container, { width }]}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.boneWhite}
+              style={styles.icon}
+            />
+          )}
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.boneWhite}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            numColumns={numberOfColumns}
+            renderItem={({ item }) => (
+              <PickerItemComponent
+                item={item}
+                label={item.label}
+                onPress={() => {
+                  setModalVisible(false);
+                  onSelectItem(item);
+                }}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.bLightBlue,
+    borderRadius: 25,
+    flexDirection: "row",
+    // width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  placeholder: {
+    color: colors.blueMarine,
+    flex: 1,
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+export default AppPicker;
