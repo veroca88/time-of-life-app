d908570365ea76f064605a4cbacc6bb534ca9581
veroca88 <veronicadonoso1506@gmail.com>
2020-06-23 23:34:18 -0400

    Create Picker type of

---
+++ app/components/AppFormPicker.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppPicker from "./AppPicker";
+import ErrorMessage from "./ErrorMessage";
+
+function AppFormPicker({
+  items,
+  name,
+  numberOfColumns = 1,
+  PickerItemComponent,
+  placeholder,
+  width,
+}) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  return (
+    <>
+      <AppPicker
+        items={items}
+        numberOfColumns={numberOfColumns}
+        onSelectItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
+        selectedItem={values[name]}
+        placeholder={placeholder}
+        width={width}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormPicker;
